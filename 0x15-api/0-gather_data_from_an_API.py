00709cb44ebd3637d42e3512e00f53f37daa37aa
Oycctem <ousstemm@gmail.com>
2024-03-25 16:00:32 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,23 +3,25 @@
 import requests
 import sys
 
+if __name__ == "__main__":
 
-employee_id = sys.argv[1]
+    employee_id = sys.argv[1]
 
-base_url = 'https://jsonplaceholder.typicode.com'
+    base_url = 'https://jsonplaceholder.typicode.com'
 
-user_response = requests.get(f'{base_url}/users/{employee_id}')
-data = user_response.json()
+    user_response = requests.get(f'{base_url}/users/{employee_id}')
+    data = user_response.json()
 
-todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
-todos_data = todos_response.json()
+    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
+    todos_data = todos_response.json()
 
-completed_tasks = [todo for todo in todos_data if todo['completed']]
-ncomplt_tasks = len(completed_tasks)
+    completed_tasks = [todo for todo in todos_data if todo['completed']]
+    ncomplt_tasks = len(completed_tasks)
 
-tasks = len(todos_data)
+    tasks = len(todos_data)
 
-print(f"Employee {data['name']} is done with tasks({ncomplt_tasks}/{tasks}):")
+    print(f"Employee {data['name']} is done with tasks", end='')
+    print(f"({ncomplt_tasks}/{tasks}):")
 
-for task in completed_tasks:
-    print(f"\t{task['title']}")
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
