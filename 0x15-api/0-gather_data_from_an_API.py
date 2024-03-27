ab8ed1d2573296c6c38711734b708ad8397bb77f
Oycctem <ousstemm@gmail.com>
2024-03-27 13:36:20 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,25 +3,30 @@
 import requests
 import sys
 
+
+def get_employee_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    user_data = requests.get(f"{base_url}/users/{employee_id}").json()
+    employee_name = user_data['name']
+
+    todo_data = requests.get(f"{base_url}/todos?userId={employee_id}").json()
+    total_tasks = len(todo_data)
+    completed_tasks = sum(1 for task in todo_data if task['completed'])
+
+    progress_message = (
+        f"Employee {employee_name} is done with tasks"
+        f"({completed_tasks}/{total_tasks}):"
+    )
+    print(progress_message)
+    for task in todo_data:
+        if task['completed']:
+            print(f"\t{task['title']}")
+
+
 if __name__ == "__main__":
-
-    employee_id = sys.argv[1]
-
-    base_url = 'https://jsonplaceholder.typicode.com'
-
-    user_response = requests.get(f'{base_url}/users/{employee_id}')
-    data = user_response.json()
-
-    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
-    todos_data = todos_response.json()
-
-    completed_tasks = [todo for todo in todos_data if todo['completed']]
-    ncomplt_tasks = len(completed_tasks)
-
-    tasks = len(todos_data)
-
-    print(f"Employee {data['name']} is done with tasks", end='')
-    print(f"({ncomplt_tasks}/{tasks}):")
-
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
